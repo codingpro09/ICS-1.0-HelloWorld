6f92de0e45c47e94e31d4ae505b2787ed0956ac6
codingpro09 <1baumannlia@hdsb.ca>
2024-09-05 19:03:40 +0000

    I said some stuff

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,12 @@
 // Log "Hello World" to the console.
-// Log four additional messages of your choosing. 
-// Comment each message appropriately. 
-console.log("Testing!");
+// Log four additional messages of your choosing.
+// Comment each message appropriately.
+console.log("Hello World! I am a JavaScript programmer.");
+console.log("I am learning JavaScript.");
+console.log("I am excited to learn more.");
+console.log(1 + 1);
+console.log(2 + 2);
+console.log(50000 * 838312873126213);
+console.log(
+  "I am so so so so so so so so so so so so so so so so so so so so so so so so so so so so awesome and very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very cool and I love to code and I am so so so so so so so so so so so so so so so so so so so so sad.",
+);
